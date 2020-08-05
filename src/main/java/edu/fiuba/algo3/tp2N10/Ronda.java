5b14c48e5b047defd3e03b148197aee6ee16a564
mabbate <mabbate@fi.uba.ar>
2020-08-05 20:03:28 -0300

    refactor

--- src/main/java/edu/fiuba/algo3/tp2N10/Ronda.java
+++ src/main/java/edu/fiuba/algo3/tp2N10/Ronda.java
@@ -1,31 +1,18 @@
-//package edu.fiuba.algo3.tp2N10;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//
-//public class Ronda {
-//    private Jugador jugadorUno;
-//    private Jugador jugadorDos;
-//    private Pregunta pregunta;
-//
-//    public Ronda(Pregunta pregunta, Jugador jugadorUno, Jugador jugadorDos){
-//        this.jugadorUno = jugadorUno;
-//        this.jugadorDos = jugadorDos;
-//        this.pregunta = pregunta;
-//    }
-//
-//    private void asignarPuntos(ArrayList<Integer> puntos){
-//        this.jugadorUno.puntuar(puntos.get(0));
-//        this.jugadorDos.puntuar(puntos.get(1));
-//    }
-//
-//    //probablemente las respuestas despues se obtengan desde un stage creado desde un metodo "ronda.iniciarRonda()"
-//    public void procesarRespuestas(ArrayList<Respuesta> respuestaUno, ArrayList<Respuesta> respuestaDos){
-//
-//
-//
-//        ArrayList<ArrayList<Respuesta>> respuestas = new ArrayList<>(Arrays.asList(respuestaUno, respuestaDos));
-//        ArrayList<Integer> puntos = this.pregunta.responder(respuestas);
-//        this.asignarPuntos(puntos);
-//    }
-//}
+package edu.fiuba.algo3.tp2N10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ronda {
+    private ArrayList<Jugador> jugadores;
+
+    public Ronda(ArrayList<Jugador> jugadores){
+        this.jugadores = jugadores;
+    }
+
+    public void asignarPuntajes(ArrayList<Integer> puntajes){
+        for (int i=0; i<puntajes.size(); i++) {
+            this.jugadores.get(i).puntuar(puntajes.get(i));
+        }
+    }
+}
