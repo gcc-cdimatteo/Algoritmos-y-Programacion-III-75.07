83267d7c20ab821c4ac5c1fceaa319d32e512065
mabbate <mabbate@fi.uba.ar>
2020-08-06 01:42:19 -0300

    Interfaz grafica basica

---
+++ src/main/java/edu/fiuba/algo3/tp2N10/Vista/App.java
@@ -0,0 +1,43 @@
+package edu.fiuba.algo3.tp2N10.Vista;
+
+
+import edu.fiuba.algo3.tp2N10.RespuestaVerdaderoFalso;
+import edu.fiuba.algo3.tp2N10.VerdaderoFalso;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    private Stage escenario;
+    private VerdaderoFalso pregunta;
+
+    public void start(Stage stage) throws Exception {
+        this.escenario = stage;
+        this.setup();
+        this.escenario.setScene(this.escenaPregunta());
+        this.escenario.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void setup(){
+        this.escenario.setTitle("AlgoKahoot");
+        this.pregunta = new VerdaderoFalso("Verde es un color",new RespuestaVerdaderoFalso(true));
+    }
+
+    //Escenas
+    public Scene escenaPregunta(){
+        Label label = new Label(this.pregunta.getEnunciado());
+        Scene escenario = new Scene(new StackPane(label), 640, 480);
+        return escenario;
+    }
+
+
+}
