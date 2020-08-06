d861d39d5be79fa6b328136c5b7716f06bfedbff
mabbate <mabbate@fi.uba.ar>
2020-08-06 17:22:21 -0300

    Primera integracion de interfaz tentativa

--- src/main/java/edu/fiuba/algo3/tp2N10/App.java
+++ src/main/java/edu/fiuba/algo3/tp2N10/App.java
@@ -1,4 +1,7 @@
-package edu.fiuba.algo3.tp2N10;
+package edu.fiuba.algo3.tp2N10.Vista;
+
+
+import edu.fiuba.algo3.tp2N10.*;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -6,22 +9,50 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 public class App extends Application {
 
-    @Override
-    public void start(Stage stage) {
+    private Stage escenario;
+    private Mostrable pregunta;
 
-        var label = new Label("Hello, JavaFX");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws Exception {
+        this.escenario = stage;
+        this.setup();
+        this.escenario.setScene(this.escenaPregunta());
+        this.escenario.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+
+    public void setup(){
+        this.escenario.setTitle("AlgoKahoot");
+        BolsaDePreguntas bolsa = null;
+        try {
+            bolsa = new BolsaDePreguntas("preguntas.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<Mostrable> preguntas = bolsa.getLista();
+        this.pregunta = preguntas.get(0);
+    }
+
+    //Escenas
+    public Scene escenaPregunta(){
+
+        String opcionesConcatenadas = new String(); // ToDo: solo para ver que sale.
+        for (String op: this.pregunta.getOpcionesPregunta()) {
+            opcionesConcatenadas  += '|' + op;
+        }
+
+        Label label = new Label(this.pregunta.getEnunciadoPregunta() + "\n" + opcionesConcatenadas);
+        Scene escenario = new Scene(new StackPane(label), 640, 480);
+        return escenario;
+    }
+
 
 }
--- src/main/java/edu/fiuba/algo3/tp2N10/BolsaDePreguntas.java
+++ src/main/java/edu/fiuba/algo3/tp2N10/BolsaDePreguntas.java
@@ -1,111 +1,113 @@
-//package edu.fiuba.algo3.tp2N10;
-//
-//import org.json.JSONArray;
-//import org.json.JSONObject;
-//
-//import java.io.*;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.HashSet;
-//
-//
-//public class BolsaDePreguntas {
-//
-//    private ArrayList<Object> lista = new ArrayList<>();
-//
-//    public BolsaDePreguntas(String archivo) throws IOException {
-//
-//        String contenido = new String(Files.readAllBytes(Paths.get(archivo)));
-//
-//        JSONObject json = new JSONObject(contenido);
-//        JSONArray preguntas = json.getJSONArray("preguntas");
-//
-//        for (int i = 0; i < preguntas.length(); i++) {
-//            JSONObject pregunta = preguntas.getJSONObject(i);
-//            switch (pregunta.getString("tipo")) {
-//                case "verdaderoFalso":
-//                    lista.add(generarPreguntaVerdaderoFalso(pregunta));
-//                    break;
-//                case "multipleChoice":
-//                    lista.add(generarPreguntaMultipleChoice(pregunta));
-//                    break;
-//                case "orderedChoice":
-//                    lista.add(generarPreguntaOrderedChoice(pregunta));
-//                    break;
-//                case "groupChoice":
-//                    lista.add(generarPreguntaGroupChoice(pregunta));
-//                    break;
-//                default:
-//                    System.out.println("Tipo de pregunta desconocida");
-//            }
-//        }
-//    }
-//
-//    private Object generarPreguntaGroupChoice(JSONObject pregunta) {
-//        System.out.println("Leyendo una pregunta GroupChoice...");
-//        // ToDo: Todavía no esta definida
-//        return null;
-//    }
-//
-//    private Object generarPreguntaOrderedChoice(JSONObject pregunta) {
-//        System.out.println("Leyendo una pregunta OrderedChoice...");
-//
-//        String enunciado = pregunta.getString("enunciado");
-//        JSONArray respuestas = pregunta.getJSONArray("respuesta");
-//
-//        ArrayList<String> arrayRespuestas = new ArrayList<>();
-//        for (int i = 0; i < respuestas.length(); i++) {
-//            arrayRespuestas.add(respuestas.getString(i));
-//        }
-//
-//        RespuestaOrderedChoice respuestaCorrecta = new RespuestaOrderedChoice(arrayRespuestas);
-//        return new OrderedChoice(enunciado, respuestaCorrecta);
-//    }
-//
-//    private Object generarPreguntaMultipleChoice(JSONObject pregunta) {
-//        System.out.println("Leyendo una pregunta Multiple Choice...");
-//        String enunciado = pregunta.getString("enunciado");
-//        JSONArray respuestas = pregunta.getJSONArray("respuesta");
-//
-//        ArrayList<String> arrayRespuestas = new ArrayList<>();
-//        for (int i = 0; i < respuestas.length(); i++) {
-//            arrayRespuestas.add(respuestas.getString(i));
-//        }
-//
-//        RespuestaMultipleChoice respuestaCorrecta = new RespuestaMultipleChoice(new HashSet<>(arrayRespuestas));
-//
-//        MultipleChoice preguntaMC = null;
-//        switch (pregunta.getString("tipoPuntaje")) {
-//            case "clasico":
-//                preguntaMC = new MultipleChoice(enunciado, respuestaCorrecta);
-//                break;
-//            case "parcial":
-//                preguntaMC = MultipleChoice.MultipleChoiceParcial(enunciado, respuestaCorrecta);
-//                break;
-//            case "conPenalidad":
-//                preguntaMC = MultipleChoice.MultipleChoicePenalidad(enunciado, respuestaCorrecta);
-//        }
-//        return preguntaMC;
-//    }
-//
-//    private Object generarPreguntaVerdaderoFalso(JSONObject pregunta) {
-//        System.out.println("Leyendo una pregunta VerdaderoFalso...");
-//        String enunciado = pregunta.getString("enunciado");
-//        boolean respuesta = pregunta.getBoolean("respuesta");
-//
-//        RespuestaVerdaderoFalso respuestaCorrecta = new RespuestaVerdaderoFalso(respuesta);
-//        VerdaderoFalso preguntaVF = null;
-//        if (pregunta.getBoolean("conPenalidad")) {
-//            preguntaVF = VerdaderoFalso.VerdaderoFalsoPenalidad(enunciado,respuestaCorrecta);
-//        } else {
-//            preguntaVF = new VerdaderoFalso(enunciado, respuestaCorrecta);
-//        }
-//        return preguntaVF;
-//    }
-//
-//    public ArrayList<Object> getLista() {
-//        return this.lista;
-//    }
-//}
+package edu.fiuba.algo3.tp2N10;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+
+public class BolsaDePreguntas {
+
+    private ArrayList<Mostrable> lista = new ArrayList<>();
+
+    public BolsaDePreguntas(String archivo) throws IOException {
+
+        String contenido = new String(Files.readAllBytes(Paths.get(archivo)));
+
+        JSONObject json = new JSONObject(contenido);
+        JSONArray preguntas = json.getJSONArray("preguntas");
+
+        for (int i = 0; i < preguntas.length(); i++) {
+            JSONObject pregunta = preguntas.getJSONObject(i);
+            switch (pregunta.getString("tipo")) {
+                case "verdaderoFalso":
+                    lista.add(generarPreguntaVerdaderoFalso(pregunta));
+                    break;
+                case "multipleChoice":
+                    lista.add(generarPreguntaMultipleChoice(pregunta));
+                    break;
+                case "orderedChoice":
+                    lista.add(generarPreguntaOrderedChoice(pregunta));
+                    break;
+                case "groupChoice":
+                    lista.add(generarPreguntaGroupChoice(pregunta));
+                    break;
+                default:
+                    System.out.println("Tipo de pregunta desconocida");
+            }
+        }
+    }
+
+    private Mostrable generarPreguntaGroupChoice(JSONObject pregunta) {
+        System.out.println("Leyendo una pregunta GroupChoice...");
+
+        // ToDo: Todavía no esta definida
+        return null;
+    }
+
+    private Mostrable generarPreguntaOrderedChoice(JSONObject pregunta) {
+        System.out.println("Leyendo una pregunta OrderedChoice...");
+
+        String enunciado = pregunta.getString("enunciado");
+        JSONArray respuestas = pregunta.getJSONArray("respuesta");
+
+        ArrayList<String> arrayRespuestas = new ArrayList<>();
+        for (int i = 0; i < respuestas.length(); i++) {
+            arrayRespuestas.add(respuestas.getString(i));
+        }
+
+        RespuestaOrderedChoice respuestaCorrecta = new RespuestaOrderedChoice(arrayRespuestas);
+        return new OrderedChoice(enunciado, respuestaCorrecta);
+    }
+
+    private Mostrable generarPreguntaMultipleChoice(JSONObject pregunta) {
+        System.out.println("Leyendo una pregunta Multiple Choice...");
+        String enunciado = pregunta.getString("enunciado");
+        JSONArray respuestas = pregunta.getJSONArray("respuesta");
+
+        ArrayList<String> arrayRespuestas = new ArrayList<>();
+        for (int i = 0; i < respuestas.length(); i++) {
+            arrayRespuestas.add(respuestas.getString(i));
+        }
+
+        RespuestaMultipleChoice respuestaCorrecta = new RespuestaMultipleChoice(new HashSet<>(arrayRespuestas));
+
+        MultipleChoice preguntaMC = null;
+        switch (pregunta.getString("tipoPuntaje")) {
+            case "clasico":
+                preguntaMC = new MultipleChoice(enunciado, respuestaCorrecta);
+                break;
+            case "parcial":
+                preguntaMC = MultipleChoice.MultipleChoiceParcial(enunciado, respuestaCorrecta);
+                break;
+            case "conPenalidad":
+                preguntaMC = MultipleChoice.MultipleChoicePenalidad(enunciado, respuestaCorrecta);
+        }
+        return preguntaMC;
+    }
+
+    private Mostrable generarPreguntaVerdaderoFalso(JSONObject pregunta) {
+        System.out.println("Leyendo una pregunta VerdaderoFalso...");
+        String enunciado = pregunta.getString("enunciado");
+        boolean respuesta = pregunta.getBoolean("respuesta");
+
+        RespuestaVerdaderoFalso respuestaCorrecta = new RespuestaVerdaderoFalso(respuesta);
+        VerdaderoFalso preguntaVF = null;
+        if (pregunta.getBoolean("conPenalidad")) {
+            preguntaVF = VerdaderoFalso.VerdaderoFalsoPenalidad(enunciado,respuestaCorrecta);
+        } else {
+            preguntaVF = new VerdaderoFalso(enunciado, respuestaCorrecta);
+        }
+        return preguntaVF;
+    }
+
+    public ArrayList<Mostrable> getLista() {
+        return this.lista;
+    }
+}
