b6818e0762b74978190b4c92eb26c90543bb40e8
Nacho <diazjignacio@gmail.com>
2020-08-05 22:44:24 -0300

    cambio pruebas tipoPuntajeClasico

--- src/main/test/edu/fiuba/algo3/tp2N10/TipoPuntajeClasicoTest.java
+++ src/main/test/edu/fiuba/algo3/tp2N10/TipoPuntajeClasicoTest.java
@@ -1,8 +1,6 @@
 package edu.fiuba.algo3.tp2N10;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -18,26 +16,27 @@ public class TipoPuntajeClasicoTest {
 
     @Test
     public void tipoPuntajeClasicoPuntuaCorrectamenteParaVariosAciertos(){
-        RespuestaMultipleChoice respuestaCorrecta = RespuestaMultipleChoice(HashSet<String>(Arrays.asList("Multiple", "Choice", "Clasico")))
-        TipoPuntajeClasico tipoPuntaje = TipoPuntajeClasico.TipoPuntajeClasicoParaMC(3);
-        assertEquals(1, tipoPuntaje.puntuar(new ArrayList<>(Arrays.asList(3,0))));
+        RespuestaMultipleChoice respuestaCorrecta = new RespuestaMultipleChoice(new HashSet<>(Arrays.asList("Multiple", "Choice", "Clasico")));
+        TipoPuntajeClasico tipoPuntaje = TipoPuntajeClasico.TipoPuntajeClasicoParaMC(respuestaCorrecta);
+        assertEquals(1, tipoPuntaje.puntuar(Arrays.asList(3,0)));
     }
 
     @Test
     public void tipoPuntajeClasicoPuntuaCorrectamenteParaVariosAciertosYUnError(){
-        TipoPuntajeClasico tipoPuntaje = TipoPuntajeClasico.TipoPuntajeClasicoConAciertosEsperados(3);
-        assertEquals(0, tipoPuntaje.puntuar(new ArrayList<>(Arrays.asList(3,1))));
+        RespuestaMultipleChoice respuestaCorrecta = new RespuestaMultipleChoice(new HashSet<>(Arrays.asList("Multiple", "Choice", "Clasico")));
+        TipoPuntajeClasico tipoPuntaje = TipoPuntajeClasico.TipoPuntajeClasicoParaMC(respuestaCorrecta);
+        assertEquals(0, tipoPuntaje.puntuar(Arrays.asList(3,1)));
     }
 
     @Test
     public void tipoPuntajeClasicoPuntuaCorrectamenteParaVariosErrores(){
         TipoPuntajeClasico tipoPuntaje = new TipoPuntajeClasico();
-        assertEquals(0, tipoPuntaje.puntuar(new ArrayList<>(Arrays.asList(0,2))));
+        assertEquals(0, tipoPuntaje.puntuar(Arrays.asList(0,2)));
     }
 
     @Test
     public void tipoPuntajeClasicoPuntuaCorrectamenteSinAciertosNiErrores(){
         TipoPuntajeClasico tipoPuntaje = new TipoPuntajeClasico();
-        assertEquals(0, tipoPuntaje.puntuar(new ArrayList<>(Arrays.asList(0,0))));
+        assertEquals(0, tipoPuntaje.puntuar(Arrays.asList(0,0)));
     }
 }
