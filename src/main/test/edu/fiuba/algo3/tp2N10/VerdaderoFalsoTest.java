4e60402c186d015a3df80dfb77cae2bf4b6febf8
Carolina <cdimatteo@fi.uba.ar>
2020-08-05 22:40:59 -0300

    test VF

--- src/main/test/edu/fiuba/algo3/tp2N10/VerdaderoFalsoTest.java
+++ src/main/test/edu/fiuba/algo3/tp2N10/VerdaderoFalsoTest.java
@@ -2,10 +2,8 @@ package edu.fiuba.algo3.tp2N10;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,32 +28,28 @@ public class VerdaderoFalsoTest {
     @Test
     public void verdaderoFalsoClasicoPuedeCrearseSiSeIndicaLaRtaCorreta(){
         crearVerdaderoFalsoClasico();
-        ArrayList<Integer> expected = new ArrayList<>(Collections.singletonList(1));
         assertEquals(Collections.singletonList(1), this.preguntaVF.responder(Collections.singletonList(this.respuestaCorrecta)));
     }
 
     @Test
-    public void verdaderoFalsoConPenalidadDevuelvePuntajeCorrecto() {
-        RespuestaVerdaderoFalso respuestaCorrecta = new RespuestaVerdaderoFalso(false);
-        RespuestaVerdaderoFalso respuestaIncorrecta = new RespuestaVerdaderoFalso(true);
-        VerdaderoFalso preguntaVF = VerdaderoFalso.VerdaderoFalsoPenalidad("La manzana es azul", respuestaCorrecta);
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(1, -1));
-        assertEquals(expected, preguntaVF.responder(new ArrayList<>(Arrays.asList(respuestaCorrecta, respuestaIncorrecta))));
+    public void verdaderoFalsoClasicoDevuelvePuntajeCorrecto() {
+        crearVerdaderoFalsoClasico();
+        assertEquals(Arrays.asList(1, 0), this.preguntaVF.responder(Arrays.asList(this.respuestaCorrecta, this.respuestaIncorrecta)));
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Penalidad
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//    @Test
-//    public void VerdaderoFalsoConPenalidadDevuelvePuntajeCorrecto(){
-//        VerdaderoFalso preguntaVF = VerdaderoFalso.VerdaderoFalsoPenalidad("La manzana es azul", false);
-//        assertEquals(-1, preguntaVF.responder(true));
-//    }
-//
-//    @Test
-//    public void VerdaderoFalsoPenalidadRecibeUnaListaDeRespuestasYAsignaPuntos(){
-//        VerdaderoFalso preguntaVF = VerdaderoFalso.VerdaderoFalsoPenalidad("La manzana es azul",false);
-//        assertEquals(new ArrayList<>(Arrays.asList(-1, 1)), preguntaVF.responderLista(new ArrayList<>(Arrays.asList(true, false))));
-//    }
+    @Test
+    public void VerdaderoFalsoConPenalidadDevuelvePuntajeCorrecto(){
+        crearVerdaderoFalsoPenalidad();
+        assertEquals(Collections.singletonList(1), this.preguntaVF.responder(Collections.singletonList(this.respuestaCorrecta)));
+    }
+
+    @Test
+    public void VerdaderoFalsoPenalidadRecibeUnaListaDeRespuestasYAsignaPuntos(){
+        crearVerdaderoFalsoPenalidad();
+        assertEquals(Arrays.asList(-1, 1), this.preguntaVF.responder(Arrays.asList(this.respuestaIncorrecta, this.respuestaCorrecta)));
+    }
 
 }
