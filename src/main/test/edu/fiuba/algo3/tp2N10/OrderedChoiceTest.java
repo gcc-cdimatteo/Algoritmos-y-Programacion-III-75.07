f94767f5d69f9aadd14fb94b1fc9b8351e720610
Nacho <diazjignacio@gmail.com>
2020-08-05 23:30:12 -0300

    agrego pruebas OrderedChoice

--- src/main/test/edu/fiuba/algo3/tp2N10/OrderedChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/tp2N10/OrderedChoiceTest.java
@@ -1,9 +1,9 @@
 package edu.fiuba.algo3.tp2N10;
 
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,18 +11,17 @@ public class OrderedChoiceTest {
 
     @Test
     public void OrderedChoicePuedeCrearseSiSeIndicanLasRtasCorretas() {
-        RespuestaOrderedChoice respuestaCorrecta = new RespuestaOrderedChoice(new ArrayList<>(Arrays.asList("A", "B", "C")));
+        RespuestaOrderedChoice respuestaCorrecta = new RespuestaOrderedChoice(Arrays.asList("A", "B", "C"));
         OrderedChoice preguntaOC = new OrderedChoice("El orden de las letras del abecedario es...", respuestaCorrecta);
-        assertEquals(Collections.singletonList(1), preguntaOC.responder(new ArrayList<>(Collections.singletonList(respuestaCorrecta))));
+        assertEquals(Collections.singletonList(1), preguntaOC.responder(Collections.singletonList(respuestaCorrecta)));
     }
 
     @Test
-    public void OrderedChoiceRecibeUnaListaDeRespuestasYAsignaPuntos() {
+    public void OrderedChoiceRecibeUnaListaDeRespuestasYAsignaPuntosCorrectamente() {
         RespuestaOrderedChoice respuestaCorrecta = new RespuestaOrderedChoice(Arrays.asList("A", "B", "C"));
-        RespuestaOrderedChoice respuestaIncorrecta = new RespuestaOrderedChoice(Arrays.asList("A", "B", "C"));
+        RespuestaOrderedChoice respuestaIncorrecta = new RespuestaOrderedChoice(Arrays.asList("A", "C", "B"));
         OrderedChoice preguntaOC = new OrderedChoice("El orden de las letras del abecedario es...", respuestaCorrecta);
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(1, 0));
+        List<Integer> expected = Arrays.asList(1, 0);
         assertEquals(expected, preguntaOC.responder(Arrays.asList(respuestaCorrecta, respuestaIncorrecta)));
     }
-
 }
