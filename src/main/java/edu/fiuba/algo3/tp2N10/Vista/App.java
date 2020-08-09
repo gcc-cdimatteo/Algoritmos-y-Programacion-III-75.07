4e7ad62b93365b61c1d31085e249f4568c8d27d9
mabbate <mabbate@fi.uba.ar>
2020-08-09 20:45:00 -0300

    codigo de ejemplo

--- src/main/java/edu/fiuba/algo3/tp2N10/Vista/App.java
+++ src/main/java/edu/fiuba/algo3/tp2N10/Vista/App.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.tp2N10.Vista;
 
 
+import edu.fiuba.algo3.tp2N10.Controlador.BotonListoProvisorio;
 import edu.fiuba.algo3.tp2N10.Modelo.AlgoKahoot.Mostrable;
 import edu.fiuba.algo3.tp2N10.Modelo.Pregunta.PreguntaMultipleChoice;
 import javafx.application.Application;
@@ -28,7 +29,7 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
         this.escenario = stage;
         this.setup();
-        this.escenario.setScene(this.escenaPregunta());
+        this.escenario.setScene(this.escenaPregunta1());
         this.escenario.show();
     }
 
@@ -44,8 +45,12 @@ public class App extends Application {
         this.pregunta = PreguntaMultipleChoice.Clasico("La manzana es...", Arrays.asList("Una Fruta", "Un Citrico", "Roja", "Azul"), respuestasCorrectas);
     }
 
+    public void cambiarEscenaA(Scene nuevaEscena) {
+        this.escenario.setScene(nuevaEscena);
+    }
+
     //Escenas
-    public Scene escenaPregunta() {
+    public Scene escenaPregunta1() {
         Label lblJugador = new Label("Jugador 1");
         lblJugador.setStyle("-fx-font-size: 200%");
         Label lblEnunciado = new Label(this.pregunta.getPregunta());
@@ -55,6 +60,56 @@ public class App extends Application {
         Button btnPowerUpEx1 = new Button("Exclusividad");
         Button btnPowerUpEx2 = new Button("Exclusividad");
         Button btnListo = new Button("Listo");
+        ////////////////////////////////////////////
+        btnListo.setOnAction(new BotonListoProvisorio(this));
+
+        ArrayList<CheckBox> arrayChkOpciones = new ArrayList<>();
+        for (String op : this.pregunta.getOpciones()) {
+            arrayChkOpciones.add(new CheckBox(op));
+        }
+
+        BorderPane bpPreguntaPowerUps = new BorderPane();
+
+        VBox vboxEnunciadoOpciones = new VBox(100);
+        vboxEnunciadoOpciones.getChildren().add(lblEnunciado);
+        VBox vboxOpciones = new VBox(25);
+        for (CheckBox chkOp : arrayChkOpciones) {
+            vboxOpciones.getChildren().addAll(chkOp);
+        }
+        vboxEnunciadoOpciones.getChildren().add(vboxOpciones);
+        VBox vboxPowerUps = new VBox(5);
+        vboxPowerUps.getChildren().addAll(btnPowerUpX2, btnPowerUpX3, btnPowerUpEx1, btnPowerUpEx2);
+        bpPreguntaPowerUps.setLeft(vboxEnunciadoOpciones);
+        bpPreguntaPowerUps.setRight(vboxPowerUps);
+
+        BorderPane bpJugadorPreguntaLista = new BorderPane();
+        bpJugadorPreguntaLista.setPadding(new Insets(10, 10, 10, 10));
+        BorderPane bpJugador = new BorderPane();
+        bpJugador.setCenter(lblJugador);
+        bpJugador.setStyle("-fx-background-color: cornflowerblue");
+        bpJugadorPreguntaLista.setTop(bpJugador);
+        bpJugadorPreguntaLista.setCenter(bpPreguntaPowerUps);
+        BorderPane bpBotoneraListo = new BorderPane();
+        bpBotoneraListo.setStyle("-fx-background-color: cornflowerblue");
+        bpBotoneraListo.setRight(btnListo);
+        bpJugadorPreguntaLista.setBottom(bpBotoneraListo);
+
+        return new Scene(bpJugadorPreguntaLista, 640, 480);
+    }
+
+    ///////////////// CODIGO DE EJEMPLO
+    public Scene escenaEjemplo() {
+        Label lblJugador = new Label("ESCENA EJEMPLO");
+        lblJugador.setStyle("-fx-font-size: 200%");
+        Label lblEnunciado = new Label(this.pregunta.getPregunta());
+
+        Button btnPowerUpX2 = new Button("x2");
+        Button btnPowerUpX3 = new Button("x3");
+        Button btnPowerUpEx1 = new Button("Exclusividad");
+        Button btnPowerUpEx2 = new Button("Exclusividad");
+        Button btnListo = new Button("Listo");
+        ////////////////////////////////////////////
+        btnListo.setOnAction(new BotonListoProvisorio(this));
 
         ArrayList<CheckBox> arrayChkOpciones = new ArrayList<>();
         for (String op : this.pregunta.getOpciones()) {
