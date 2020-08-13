5f2d97d3b11801a97e5558501cc28960b05433b3
Carolina <cdimatteo@fi.uba.ar>
2020-08-13 19:24:26 -0300

    uml: agrego diagrama de clases

--- src/main/java/edu/fiuba/algo3/tp2N10/Modelo/AlgoKahoot/AlgoKahoot.java
+++ src/main/java/edu/fiuba/algo3/tp2N10/Modelo/AlgoKahoot/AlgoKahoot.java
@@ -1,10 +1,12 @@
 package edu.fiuba.algo3.tp2N10.Modelo.AlgoKahoot;
 
+import edu.fiuba.algo3.tp2N10.Modelo.BolsaDePreguntas;
 import edu.fiuba.algo3.tp2N10.Modelo.Excepciones.JuegoTerminadoException;
 import edu.fiuba.algo3.tp2N10.Modelo.Pregunta.Pregunta;
 import edu.fiuba.algo3.tp2N10.Modelo.Puntuadores.Puntuador;
 import edu.fiuba.algo3.tp2N10.Modelo.Respuesta.Respuesta;
 
+import java.io.IOException;
 import java.util.List;
 
 public class AlgoKahoot {
@@ -33,4 +35,5 @@ public class AlgoKahoot {
             throw new JuegoTerminadoException();
         }
     }
+
 }
