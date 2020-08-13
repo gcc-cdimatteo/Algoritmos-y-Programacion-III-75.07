daf3d4fc79d7e6fdfd3c8a5e92646be62683b742
Nacho <diazjignacio@gmail.com>
2020-08-13 18:23:01 -0300

    chore: arreglo AlgoKahoot

--- src/main/java/edu/fiuba/algo3/tp2N10/Modelo/AlgoKahoot/AlgoKahoot.java
+++ src/main/java/edu/fiuba/algo3/tp2N10/Modelo/AlgoKahoot/AlgoKahoot.java
@@ -1,12 +1,36 @@
 package edu.fiuba.algo3.tp2N10.Modelo.AlgoKahoot;
 
+import edu.fiuba.algo3.tp2N10.Modelo.Excepciones.JuegoTerminadoException;
 import edu.fiuba.algo3.tp2N10.Modelo.Pregunta.Pregunta;
 import edu.fiuba.algo3.tp2N10.Modelo.Puntuadores.Puntuador;
 import edu.fiuba.algo3.tp2N10.Modelo.Respuesta.Respuesta;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AlgoKahoot {
-    
+
+    private List<Jugador> jugadores;
+    private final List<Pregunta> preguntas;
+    private Integer preguntaActual = 0;
+    private Ronda rondaActual;
+
+    public AlgoKahoot(List<Pregunta> preguntas, List<Jugador> jugadores) {
+        this.preguntas = preguntas;
+        this.jugadores = jugadores;
+        rondaActual = new Ronda(preguntas.get(preguntaActual), jugadores);
+    }
+
+    public Ronda getRonda() {
+        return rondaActual;
+    }
+
+    public void siguienteRonda() {
+        preguntaActual++;
+        rondaActual.asignarPuntos();
+        try {
+            rondaActual = new Ronda(preguntas.get(preguntaActual), jugadores);
+        } catch (IndexOutOfBoundsException e) {
+            throw new JuegoTerminadoException();
+        }
+    }
 }
