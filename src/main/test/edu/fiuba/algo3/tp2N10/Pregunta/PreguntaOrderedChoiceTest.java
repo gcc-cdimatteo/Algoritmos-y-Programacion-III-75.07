13cc30567111e2edc1d7a4bcd714b06c919ea04c
Carolina <cdimatteo@fi.uba.ar>
2020-08-24 19:50:57 -0300

    fix: test de OrderedChoice mal escritas

--- src/main/test/edu/fiuba/algo3/tp2N10/Pregunta/PreguntaOrderedChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/tp2N10/Pregunta/PreguntaOrderedChoiceTest.java
@@ -17,7 +17,7 @@ public class PreguntaOrderedChoiceTest {
     public PreguntaOrderedChoiceTest() {
         String enunciado = "El orden de las letras del abecedario es...";
         List<String> opciones = Arrays.asList("A", "B", "C");
-        this.opcionesCorrectas = Arrays.asList(1, 2, 3);
+        this.opcionesCorrectas = Arrays.asList(0, 1, 2);
         this.preguntaOC = new PreguntaOrderedChoice(enunciado, opciones, this.opcionesCorrectas);
     }
 
@@ -29,14 +29,14 @@ public class PreguntaOrderedChoiceTest {
 
     @Test
     public void test02UnaPreguntaOCPuntua0SiSeRespondeMal() {
-        RespuestaOrderedChoice unaRespuesta = new RespuestaOrderedChoice(Arrays.asList(3, 2, 1));
+        RespuestaOrderedChoice unaRespuesta = new RespuestaOrderedChoice(Arrays.asList(2, 1, 0));
         assertEquals(Collections.singletonList(0), this.preguntaOC.responder(Collections.singletonList(unaRespuesta)));
     }
 
     @Test
     public void test03UnaPreguntaOCRecibeUnaListaDeRespuestasYAsignaPuntos() {
         RespuestaOrderedChoice respuestaCorrecta = new RespuestaOrderedChoice(this.opcionesCorrectas);
-        RespuestaOrderedChoice respuestaIncorrecta = new RespuestaOrderedChoice(Arrays.asList(3, 2, 1));
+        RespuestaOrderedChoice respuestaIncorrecta = new RespuestaOrderedChoice(Arrays.asList(2, 1, 0));
         assertEquals(Arrays.asList(1, 0), this.preguntaOC.responder(Arrays.asList(respuestaCorrecta, respuestaIncorrecta)));
     }
 }
